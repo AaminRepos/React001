97d8c0dcd297ca7a6b39847d6b7ef04313121921
Amins <ahmedtawfik.amin@gmail.com>
2023-09-15 17:53:20 +0300

    fixed group list and added fragments

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,9 @@ import ListGroup from "./components/ListGroup";
 
 function App() {
   return (
-    <ul className="list-group">
-      <li className="list-group-item active">1st</li>
-      <li className="list-group-item">2nd</li>
-      <li className="list-group-item">3rd</li>
-      <li className="list-group-item">4th</li>
-      <li className="list-group-item">5th</li>
-    </ul>
+    <div>
+      <ListGroup />
+    </div>
   );
 }
-
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,21 @@
-function ListGroup(){
-    return <h1>ListGroup</h1>
+//import { Fragment } from "react"; or nothing here 
+//and keep the first tags empty
+
+function ListGroup() {
+  return (
+    //<Fragment>
+    <>
+        <h1>List</h1>
+      <ul className="list-group">
+        <li className="list-group-item active">1st</li>
+        <li className="list-group-item">2nd</li>
+        <li className="list-group-item">3rd</li>
+        <li className="list-group-item">4th</li>
+        <li className="list-group-item">5th</li>
+      </ul>
+    </>
+    //</Fragment>
+  );
 }
 
 export default ListGroup;
