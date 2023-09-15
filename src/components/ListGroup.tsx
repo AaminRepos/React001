95d9e39a1fe9254fa4e0a7fd3d339aac181fc1e9
Amins <ahmedtawfik.amin@gmail.com>
2023-09-15 18:06:12 +0300

    list rendering with keys

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,19 @@
-//import { Fragment } from "react"; or nothing here 
+//import { Fragment } from "react"; or nothing here
 //and keep the first tags empty
 
 function ListGroup() {
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  items.map((item) => <li>{item}</li>);
+
   return (
     //<Fragment>
     <>
-        <h1>List</h1>
+      <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item active">1st</li>
-        <li className="list-group-item">2nd</li>
-        <li className="list-group-item">3rd</li>
-        <li className="list-group-item">4th</li>
-        <li className="list-group-item">5th</li>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </>
     //</Fragment>
