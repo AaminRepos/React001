930bdfa5a14d71008d2cf32aee26e5219d8bfcb7
Amins <ahmedtawfik.amin@gmail.com>
2023-09-17 18:16:45 +0300

    passing data via props

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -4,8 +4,13 @@
 
 import { useState } from "react";
 
-function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+interface Props {
+  items: string[];
+  heading: string;
+}
+
+
+function ListGroup({items, heading}: Props) {
 
   //hook//
   const [selectedIndex, setsSelectedIndex] = useState(-1);
@@ -20,7 +25,7 @@ function ListGroup() {
   return (
     //<Fragment>
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
 
       {items.length === 0 && <p>No items found</p>}
 
