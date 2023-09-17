bc541afebe8a1f471efe36eceea4e0960ab0228f
Amins <ahmedtawfik.amin@gmail.com>
2023-09-17 17:41:02 +0300

    events handling

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,26 +1,34 @@
 //import { Fragment } from "react"; or nothing here
 //and keep the first tags empty
+import { MouseEvent } from "react";
+
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
 
-  const message = {}
+    //event handler//
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   //const getMessage = () => {
-    //return items.length ===0 ? <p>No tems Found</p>: null;;
+  //return items.length ===0 ? <p>No tems Found</p>: null;;
   //}
 
   return (
     //<Fragment>
     <>
       <h1>List</h1>
-      
-      {items.length === 0 && <p>No items found</p> }
+
+      {items.length === 0 && <p>No items found</p>}
 
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={handleClick}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
